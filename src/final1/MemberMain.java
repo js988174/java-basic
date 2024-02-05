abcd9a47c9cc322cea58821c9b33a1c009d445fc
js988174 <76925402+js988174@users.noreply.github.com>
2024-02-05 14:48:44 +0900

    final

---
+++ src/final1/MemberMain.java
@@ -0,0 +1,14 @@
+package final1;
+
+public class MemberMain {
+
+
+    public static void main(String[] args) {
+        Member member = new Member("myId", "kim");
+        member.print();
+        member.changeData("gim");
+        member.print();
+
+
+    }
+}
