2d0955c51806639c440f0c478c35aea75778d78d
js988174 <76925402+js988174@users.noreply.github.com>
2024-02-06 10:14:09 +0900

    다형성

---
+++ src/poly/basic/PolyMain.java
@@ -0,0 +1,11 @@
+package poly.basic;
+
+public class PolyMain {
+
+
+    public static void main(String[] args) {
+        System.out.println("Parent -> Parent");
+        Parent parent = new Parent();
+        parent.parentMethod();
+    }
+}
